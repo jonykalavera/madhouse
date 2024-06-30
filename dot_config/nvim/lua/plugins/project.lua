638493ab15d3d58255f83dc0c1aaa339ea2cc2b9
Jony Kalavera <mr.jony@gmail.com>
2024-06-30 01:26:30 -0600

    some fixes for project

--- dot_config/nvim/lua/plugins/project.lua
+++ dot_config/nvim/lua/plugins/project.lua
@@ -5,7 +5,17 @@ return {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
-			exclude_dirs = { "/home/jony" },
+			patterns = {
+				".git",
+				"_darcs",
+				".hg",
+				".bzr",
+				".svn",
+				"Makefile",
+				"package.json",
+				"lazy-lock.json",
+				"*.csproj",
+			},
 		})
 		require("telescope").load_extension("projects")
 		vim.keymap.set("n", "<leader>pp", function()
