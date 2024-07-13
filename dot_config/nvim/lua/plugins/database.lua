599b550ec8fd428c7b79232816a84ba9997e9eef
Jony Kalavera <mr.jony@gmail.com>
2024-07-13 03:07:03 -0600

    Auto-add-all 💀 from madhouse-desktop

--- dot_config/nvim/lua/plugins/database.lua
+++ dot_config/nvim/lua/plugins/database.lua
@@ -1,5 +1,10 @@
 return {
 	"tpope/vim-dadbod",
 	"kristijanhusak/vim-dadbod-completion",
-	"kristijanhusak/vim-dadbod-ui",
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		config = function()
+			vim.keymap.set("n", "<Leader>da", ":DBUI<CR>", { desc = "DadBod UI" })
+		end,
+	},
 }
